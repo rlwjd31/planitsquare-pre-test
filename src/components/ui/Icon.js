76fb635c7347d95718c20c0ee437ede9aa74cb29
rlwjd31 <rlwjd31@naver.com>
2025-06-26 06:42:25 +0900

    📝 [Docs]: Check icon props jsdoc 작성

--- src/components/ui/Icon.js
+++ src/components/ui/Icon.js
@@ -6,6 +6,13 @@ const iconMap = {
   add: "./src/assets/add.svg",
 };
 
+/**
+ * 아이콘 컴포넌트
+ * @param {Object} props
+ * @param {'delete' | 'check' | 'calendar' | 'filter' | 'add'} props.variant - 아이콘 종류
+ * @param {string} props.size - 아이콘 크기 (e.g. '24px', '1rem')
+ * @returns {HTMLImageElement} 
+ */
 export default function Icon({ variant, size }) {
   this.$icon = document.createElement("img");
 
