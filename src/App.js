046bc0830c1a71b5cae226839d33fa9743522199
rlwjd31 <rlwjd31@naver.com>
2025-06-26 00:32:50 +0900

    🔨 [Chore]: 프로젝트 초기 및 파일 구조 설정
    - src폴더에서 관리
    - module type javascript로 설정
    - git message template 설정
    - App component에서 button 테스트

---
+++ src/App.js
@@ -0,0 +1,32 @@
+export default function App() {
+  this.state = {}; // todo state 초기화
+
+  this.init = () => {
+    // TODO: 나중에 state persistence기능 구현하기
+  };
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    const $root = document.getElementById("app");
+    const $main = document.createElement("main");
+    const $button = document.createElement("button");
+    $button.textContent = "button test";
+    $button.addEventListener("click", () => {
+      alert("button clicked");
+    });
+
+    $main.appendChild($button);
+
+    $root.appendChild($main);
+  };
+
+  this.init();
+  this.setState({});
+  // this.render(); 👉🏻 setState에서 한 번 rendering이 되므로 주석처리
+
+  return document.createElement("div");
+}
